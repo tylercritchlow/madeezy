f8550da28987e61a19a39adf7f68b1d4e4d3ef7e
Tyler Critchlow <tylerdarincritchlow@gmail.com>
2023-10-30 20:32:23 -0600

    add button tween animation, on trigger exit and enter

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ButtonScript : MonoBehaviour
 {
@@ -19,13 +20,39 @@ public class ButtonScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Touching player");
+        // Debug.Log("Touching player");
         BroadcastMessage("HandleButtonPress", gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        BroadcastMessage("HandleButtonLeave", gameObject);
     }
 
     void HandleButtonPress(GameObject gameObject)
     {
         Debug.Log(gameObject.transform.position);
+        
+        Transform ParentObject = gameObject.transform.parent;
+        Vector3 TweenPositionStart = gameObject.transform.parent.position;
+
+        DOTween.Init();
+        ParentObject.transform.DOMoveY(TweenPositionStart.y - 0.2f, 3);
     }
+
+    void HandleButtonLeave(GameObject gameObject)
+    {
+        Transform ParentObject = gameObject.transform.parent;
+        Vector3 TweenPositionStart = gameObject.transform.parent.position;
+
+        DOTween.Init();
+        ParentObject.transform.DOMoveY(TweenPositionStart.y + 0.2f, 3);
+    }
+
+    // Above code gets called when any Button is pressed, handle accorodingly.
+
+
 }
+
+
 
